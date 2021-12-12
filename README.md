4320e7c92abbed38ac70d9bded49ce93bd7db24a
mattlockyer <mattdlockyer@gmail.com>
2021-12-12 13:38:16 -0800

    fix test

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Durable Objects TypeScript Counter template
 
 
+# Experimentatal Boilerplate
+
+```
+yarn && yarn start
+// another terminal
+yarn test-local
+```
+
 ## Note: You must use [wrangler](https://developers.cloudflare.com/workers/cli-wrangler/install-update) 1.19.3 or newer to use this template.
 
 ## Please read the [Durable Object documentation](https://developers.cloudflare.com/workers/learning/using-durable-objects) before using this template.
--- test/app.test.js
+++ test/app.test.js
@@ -8,10 +8,15 @@ const fetchJson = (url, options) => fetch(url, options).then((r) => r.json());
 const fetchText = (url, options) => fetch(url, options).then((r) => r.text());
 
 test('should return', async (t) => {
-	await Promise.all(Array(RUNS).fill(0).map(async () => {
+	let first, last
+	await Promise.all(Array(RUNS).fill(0).map(async (_, i) => {
 		const res = await fetchText(`${HOST}/increment?key=test`, {
 			method: 'GET',
 		});
-		console.log(res);
+		const num = parseInt(res)
+		if (num % 100 === 1) first = parseInt(res)
+		if (num % 100 === 0) last = parseInt(res)
 	}))
+
+	t.is(last - first, 99)
 });
